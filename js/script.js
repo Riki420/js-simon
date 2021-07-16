a28f5a9d627bdb2d476af628378ed5b95670dc11
Riki420 <girottiriccardo1998@gmail.com>
2021-07-16 14:08:27 +0200

    boilerplate + link js + link css

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+
+//--------------------------------------------------------------------------------------//
+//                                                                                      //
+//                                  ESERCIZIO JS-SIMON                                  //
+//                                                                                      //
+//--------------------------------------------------------------------------------------//
+
+
+
+// ┌──────────────────────────────────────────────────────────────────────────────┐
+// │ Un alert() espone 5 numeri generati casualmente. (Decidete voi se debbano    │
+// │ essere tutti                                                                 │
+// │ diversi)                                                                     │
+// │ Non appena l'utente schiaccia "ok", parte un timer di 30 secondi. (Bonus:    │
+// │ visualizzare il                                                              │
+// │ timer)                                                                       │
+// │ Al termine dei 30 secondi l'utente deve inserire, uno alla volta, i numeri   │
+// │ che ha visto precedentemente, tramite il prompt(). (Bonus: far inserire i    │
+// │ numeri da un                                                                 │
+// │ form)                                                                        │
+// │ Dopo che sono stati inseriti i 5 numeri, il software mostra in un alert      │
+// │ quanti e quali dei numeri da indovinare sono stati individuati. (Bonus:      │
+// │ stampare in pagina il risultato, in alternativa                              │
+// │ all'alert.)                                                                  │
+// └──────────────────────────────────────────────────────────────────────────────┘
+
+
+
+//* 1 Genero 5 numeri casuali e li mostro con un alert
+//* 2 Quando viene chiuso questo alert, parte il timer di 30 secondi
+//* 3 Dopo 30 secondi, creo un prompt per far inserire i numeri all'utente
+//* 4 Stabilisco se i numeri dell'utente coincidono con quelli iniziali
+
+
+
